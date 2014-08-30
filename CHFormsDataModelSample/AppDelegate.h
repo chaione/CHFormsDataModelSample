a570595c9155e769bb4a6fa0d9b9bc37b7c2dc6e
Yuichi Fujiki <yuichi@chaione.com>
2014-08-30 12:52:27 -0500

    Initial commit (generated by xgen)

---
+++ CHFormsDataModelSample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CHFormsDataModelSample
+//
+//  Created by Yuichi Fujiki on 2/7/14.
+//  Copyright (c) 2014 chaione. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
